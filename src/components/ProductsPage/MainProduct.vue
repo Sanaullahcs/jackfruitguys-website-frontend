0011cc6075c764c6ee1e4dc18dbfa9d3c209e478
Zain Malik <116706625+zainmalik33543@users.noreply.github.com>
2024-09-02 11:39:44 -0700

    Products

--- src/components/ProductsPage/MainProduct.vue
+++ src/components/ProductsPage/MainProduct.vue
@@ -1,5 +1,57 @@
 <template>
+  <div>
     <div>
-        main product
+      <h1>Our Products</h1>
+      <p>
+        Welcome to Jackfruit Guys, where we offer a variety of premium jackfruit
+        products to delight your taste buds and support your healthy lifestyle.
+        Explore our selection and discover the versatility and benefits of
+        jackfruit in every bite.
+      </p>
     </div>
+    <div>
+      <h1>Jackfruit Meat</h1>
+      <p>Unleash the Power of Plants: Our Jackfruit Meat Substitute.</p>
+      <p>
+        Looking for a delicious, versatile, and sustainable meat alternative?
+        Look no further than our number one product, the incredible jackfruit
+        meat substitute!
+      </p>
+    </div>
+    <div>
+      <v-row>
+        <v-col cols="6">
+          <div>
+            <ol>
+              <li>
+                <b>High in Fiber:</b> Jackfruit is a fantastic source of dietary
+                fiber, crucial for gut health and promoting a feeling of
+                fullness. Fiber keeps you feeling satisfied for longer, aiding
+                in weight management and digestion
+              </li>
+              <li>
+                <b>Rich in Vitamins and Minerals:</b> Jackfruit boasts a treRich
+                in Vitamins and Minerals:asure trove of essential vitamins and
+                minerals. It's a good source of Vitamin C, which supports your
+                immune system, and potassium, which helps regulate blood
+                pressure. Additionally, jackfruit contains magnesium, folate,
+                and vitamin B6
+              </li>
+              <li>
+                <b>Low in Calories and Fat:</b> Jackfruit is naturally low in
+                calories and fat, making it a perfect choice for weight
+                management or healthy eating plans. This allows you to enjoy
+                satisfying meals without compromising your dietary goals
+              </li>
+            </ol>
+          </div>
+        </v-col>
+        <v-col cols="6">
+          <div>
+            <v-img src="../../assets/images/ben1.png"></v-img>
+          </div>
+        </v-col>
+      </v-row>
+    </div>
+  </div>
 </template>
