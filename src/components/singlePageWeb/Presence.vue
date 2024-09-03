6f6cd5dfb225ed908a61b2b4e5277cbf5239efe0
Zain Malik <116706625+zainmalik33543@users.noreply.github.com>
2024-09-03 09:06:54 -0700

    Structuring the presence page

--- src/components/singlePageWeb/Presence.vue
+++ src/components/singlePageWeb/Presence.vue
@@ -1,29 +1,30 @@
 <template>
-    <div  class="mt-75" style="background-color: white">
-      <NavbarCom />
-      <HeaderCom />
-      <ChatBot />
-      <FooterCom />
-    </div>
-  </template>
-  
-  <script>
-  import NavbarCom from "../NavbarCom.vue";
-  import FooterCom from "../AppFooter.vue";
-  import HeaderCom from "../PresencePage/Header.vue";
-  import ChatBot from "../ChatBot/ChatBot.vue";
-  
-  export default {
-    components: {
-      NavbarCom,
-      HeaderCom,
-      FooterCom,
-      ChatBot,
-      
-    },
-  };
-  </script>
-  <style>
-  @import "@/assets/css/style.css";
-  </style>
-  
+  <div class="mt-75" style="background-color: white">
+    <NavbarCom />
+    <HeaderCom />
+    <MainPresence />
+    <ChatBot />
+    <FooterCom />
+  </div>
+</template>
+
+<script>
+import NavbarCom from "../NavbarCom.vue";
+import MainPresence from "../PresencePage/MainPresence.vue";
+import FooterCom from "../AppFooter.vue";
+import HeaderCom from "../PresencePage/Header.vue";
+import ChatBot from "../ChatBot/ChatBot.vue";
+
+export default {
+  components: {
+    NavbarCom,
+    MainPresence,
+    HeaderCom,
+    FooterCom,
+    ChatBot,
+  },
+};
+</script>
+<style>
+@import "@/assets/css/style.css";
+</style>
