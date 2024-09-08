252d82ed5931371cbd9273fb0832a3dc55495e32
Zain Malik <116706625+zainmalik33543@users.noreply.github.com>
2024-09-08 00:12:25 -0700

    Update MainBenefits.vue

--- src/components/BenefitsPage/MainBenefits.vue
+++ src/components/BenefitsPage/MainBenefits.vue
@@ -11,7 +11,7 @@
       eco-friendly farming and local communities.
     </p>
   </div>
-  <div class="text-black px-16">
+  <div class="text-black pink-bg px-16">
     <div class="py-10">
       <h1 class="jackfruitsHeading max-700 mx-auto">
         Jackfruit: A Meat Substitute Packed with Health Benefits.
@@ -24,17 +24,17 @@
       </p>
     </div>
     <div>
-      <v-row>
+      <v-row no-gutters>
         <v-col cols="6">
-          <div>
-            <ol class="jackfruitsText">
-              <li>
+          <div class="pr-3">
+            <ul class="jackfruitsText">
+              <li class="my-5">
                 <b>High in Fiber:</b> Jackfruit is a fantastic source of dietary
                 fiber, crucial for gut health and promoting a feeling of
                 fullness. Fiber keeps you feeling satisfied for longer, aiding
                 in weight management and digestion
               </li>
-              <li>
+              <li class="mb-5">
                 <b>Rich in Vitamins and Minerals:</b> Jackfruit boasts a treRich
                 in Vitamins and Minerals:asure trove of essential vitamins and
                 minerals. It's a good source of Vitamin C, which supports your
@@ -42,13 +42,13 @@
                 pressure. Additionally, jackfruit contains magnesium, folate,
                 and vitamin B6
               </li>
-              <li>
+              <li class="mb-5">
                 <b>Low in Calories and Fat:</b> Jackfruit is naturally low in
                 calories and fat, making it a perfect choice for weight
                 management or healthy eating plans. This allows you to enjoy
                 satisfying meals without compromising your dietary goals
               </li>
-            </ol>
+            </ul>
           </div>
         </v-col>
         <v-col cols="6">
@@ -76,95 +76,95 @@
           </div>
         </v-col>
         <v-col cols="6">
-          <div>
-            <ol class="jackfruitsText">
-              <li>
+          <div class="pr-3">
+            <ul class="jackfruitsText">
+              <li class="my-5">
                 <b>Potential Blood Sugar Management:</b> Studies suggest that
                 jackfruit possess properties that help regulate blood sugar
                 levels. Its fiber content contributes to this effect by slowing
                 down the absorption of sugar in the bloodstream.
               </li>
-              <li>
+              <li class="mb-5">
                 <b> Aids in Heart Health:</b> The presence of fiber and
                 potentially antioxidants in jackfruit contributes to heart
                 health by lowering LDL (bad) cholesterol levels
               </li>
-              <li>
+              <li class="mb-5">
                 <b>Versatile for Diverse Diets:</b> Jackfruit's neutral flavor
                 and meaty texture make it adaptable to various dietary needs.
                 Vegans, vegetarians, and those seeking gluten-free options can
                 enjoy jackfruit as a satisfying and nutritious substitute
               </li>
-            </ol>
+            </ul>
           </div>
         </v-col>
       </v-row>
     </div>
+  </div>
+  <div class="bg-white px-16">
+    <div class="py-10">
+      <h1 class="jackfruitsHeading max-700 mx-auto">
+        Social Benefits of our mexican Jackfruit.
+      </h1>
+      <p class="jackfruitsText max-700 mx-auto text-center">
+        Jackfruit boasts a range of social benefits that make it a responsible
+        and impactful food choice. Here's how including jackfruit in your diet
+        contributes to a better world:
+      </p>
+    </div>
     <div>
-      <div>
-        <h1 class="jackfruitsHeading">
-          Social Benefits of our mexican Jackfruit.
-        </h1>
-        <p class="jackfruitsText">
-          Jackfruit boasts a range of social benefits that make it a responsible
-          and impactful food choice. Here's how including jackfruit in your diet
-          contributes to a better world:
-        </p>
-      </div>
-      <div>
-        <v-row>
-          <v-col cols="6">
-            <div>
-              <ol class="jackfruitsText">
-                <li>
-                  <b>Supports Sustainable Farming:</b> Jackfruit trees are
-                  incredibly productive, yielding high quantities of fruit with
-                  minimal land use compared to traditional meat production. This
-                  translates to less deforestation, soil erosion, and resource
-                  depletion.
-                </li>
-                <li>
-                  <b>Empowers Local Communities:</b> Jackfruit cultivation is a
-                  significant source of income for local Mexican farmers.
-                  Choosing jackfruit supports these communities and encourages
-                  sustainable agricultural practices.
-                </li>
-                <li>
-                  <b>Promotes Food Security:</b> Jackfruit's versatility and
-                  long shelf life make it a valuable food source in regions
-                  facing food insecurity. Its drought tolerance further
-                  strengthens its role in ensuring consistent food availability.
-                </li>
-                <li>
-                  <b>Reduces Reliance on Factory Farming:</b> The rise of
-                  jackfruit consumption lessens the demand for factory-farmed
-                  meats, which are often associated with unethical animal
-                  treatment and environmental pollution.
-                </li>
-                <li>
-                  <b>Creates Job Opportunities:</b> The growing popularity of
-                  jackfruit creates new job opportunities throughout the supply
-                  chain, from harvesting and processing to distribution and
-                  preparation. This fosters economic development in areas where
-                  jackfruit is cultivated.
-                </li>
-              </ol>
-            </div>
-          </v-col>
-          <v-col cols="6">
-            <div>
-              <v-img src="../../assets/images/ben3.png"></v-img>
-              <p class="jackfruitsText">
-                By incorporating jackfruit into your diet, you're not just
-                making a delicious choice, you're contributing to a more <span
-                  class="jackfruitsTextGreen"
-                  >sustainable, equitable, and food-secure future!</span
-                >
-              </p>
-            </div>
-          </v-col>
-        </v-row>
-      </div>
+      <v-row>
+        <v-col cols="6">
+          <div>
+            <ul class="jackfruitsText">
+              <li class="my-5">
+                <b>Supports Sustainable Farming:</b> Jackfruit trees are
+                incredibly productive, yielding high quantities of fruit with
+                minimal land use compared to traditional meat production. This
+                translates to less deforestation, soil erosion, and resource
+                depletion.
+              </li>
+              <li class="my-5">
+                <b>Empowers Local Communities:</b> Jackfruit cultivation is a
+                significant source of income for local Mexican farmers. Choosing
+                jackfruit supports these communities and encourages sustainable
+                agricultural practices.
+              </li>
+              <li class="my-5">
+                <b>Promotes Food Security:</b> Jackfruit's versatility and long
+                shelf life make it a valuable food source in regions facing food
+                insecurity. Its drought tolerance further strengthens its role
+                in ensuring consistent food availability.
+              </li>
+              <li class="my-5">
+                <b>Reduces Reliance on Factory Farming:</b> The rise of
+                jackfruit consumption lessens the demand for factory-farmed
+                meats, which are often associated with unethical animal
+                treatment and environmental pollution.
+              </li>
+              <li class="my-5">
+                <b>Creates Job Opportunities:</b> The growing popularity of
+                jackfruit creates new job opportunities throughout the supply
+                chain, from harvesting and processing to distribution and
+                preparation. This fosters economic development in areas where
+                jackfruit is cultivated.
+              </li>
+            </ul>
+          </div>
+        </v-col>
+        <v-col cols="6">
+          <div>
+            <v-img src="../../assets/images/ben3.png"></v-img>
+            <p class="jackfruitsText mb-16">
+              By incorporating jackfruit into your diet, you're not just making
+              a delicious choice, you're contributing to a more <span
+                class="jackfruitsTextGreen"
+                >sustainable, equitable, and food-secure future!</span
+              >
+            </p>
+          </div>
+        </v-col>
+      </v-row>
     </div>
   </div>
 </template>
@@ -173,5 +173,8 @@
   background-image: url(../../assets/images/maxFooter.png);
   background-position: bottom left;
   background-size: 12%;
+}
+.pink-bg {
+  background-color: #fef9eb;
 }
 </style>
