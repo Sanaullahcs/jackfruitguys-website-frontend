b8a7c83e94015ce599ee1f60dfd206773d1bc1c5
Zain Malik <116706625+zainmalik33543@users.noreply.github.com>
2024-09-08 09:57:49 -0700

    Gallery

--- src/components/singlePageWeb/Experience.vue
+++ src/components/singlePageWeb/Experience.vue
@@ -1,29 +1,30 @@
 <template>
-    <div  class="mt-75" style="background-color: white">
-      <!-- <NavbarCom /> -->
-      <HeaderCom />
-      <ChatBot />
-      <FooterCom />
-    </div>
-  </template>
-  
-  <script>
-  // import NavbarCom from "../NavbarCom.vue";
-  import FooterCom from "../AppFooter.vue";
-  import HeaderCom from "../ExperiencePage/Header.vue";
-  import ChatBot from "../ChatBot/ChatBot.vue";
-  
-  export default {
-    components: {
-      // NavbarCom,
-      HeaderCom,
-      FooterCom,
-      ChatBot,
-      
-    },
-  };
-  </script>
-  <style>
-  @import "@/assets/css/style.css";
-  </style>
-  
+  <div class="mt-75" style="background-color: white">
+    <!-- <NavbarCom /> -->
+    <HeaderCom />
+    <MainExperience />
+    <ChatBot />
+    <FooterCom />
+  </div>
+</template>
+
+<script>
+// import NavbarCom from "../NavbarCom.vue";
+import FooterCom from "../AppFooter.vue";
+import HeaderCom from "../ExperiencePage/Header.vue";
+import MainExperience from "../ExperiencePage/MainExperience.vue";
+import ChatBot from "../ChatBot/ChatBot.vue";
+
+export default {
+  components: {
+    // NavbarCom,
+    HeaderCom,
+    FooterCom,
+    MainExperience,
+    ChatBot,
+  },
+};
+</script>
+<style>
+@import "@/assets/css/style.css";
+</style>
