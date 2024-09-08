370f7e748e614ad06290fc96278415240fad8cf6
Zain Malik <116706625+zainmalik33543@users.noreply.github.com>
2024-09-08 01:11:33 -0700

    Update MainRecipes.vue

--- src/components/RecipesPage/MainRecipes.vue
+++ src/components/RecipesPage/MainRecipes.vue
@@ -1,8 +1,8 @@
 <template>
-  <div class="text-black">
-    <div>
-      <h1 class="jackfruitsHeading">Recipes</h1>
-      <p class="jackfruitsText">
+  <div class="text-black py-10">
+    <div class="maxFooter">
+      <h1 class="jackfruitsHeading max-700 mx-auto">Recipes</h1>
+      <p class="jackfruitsText max-700 mx-auto text-center pb-10">
         Explore the delicious possibilities with jackfruit through our curated
         collection of recipes. Whether you’re looking for savory dishes, sweet
         treats, or innovative snacks, the Jackfruit Guys family has something
@@ -13,122 +13,127 @@
         journey and discover how jackfruit can transform your cooking!
       </p>
     </div>
-    <div>
+    <div class="pink-bg pa-16">
       <div>
-        <v-img src="../../assets/images/res1.png"></v-img>
+        <div class="my-3">
+          <v-img src="../../assets/images/res1.png"></v-img>
+        </div>
+        <div>
+          <h3 class="jackfruitsHeadingSmall">
+            Pulled Jackfruit BBQ Sandwiches
+          </h3>
+          <p class="jackfruitsText">Intructions:</p>
+          <ol class="jackfruitsText px-4">
+            <li>
+              Prepare the Jackfruit: Drain and rinse the jackfruit, then cut it
+              into smaller pieces. Remove any hard seeds and set the jackfruit
+              aside.
+            </li>
+            <li>
+              Cook the Onion and Garlic: In a large skillet, heat olive oil over
+              medium heat. Add the sliced onions and cook until soft and
+              caramelized, about 5-7 minutes. Add the minced garlic and cook for
+              another 1-2 minutes.
+            </li>
+            <li>
+              Add Spices and Jackfruit: Stir in the smoked paprika, chili
+              powder, cumin, salt, and pepper. Add the jackfruit pieces and toss
+              to coat them in the spice mixture.
+            </li>
+            <li>
+              Simmer in BBQ Sauce: Pour in the BBQ sauce, mixing well to ensure
+              the jackfruit is evenly coated. Reduce the heat and let it simmer
+              for 20-30 minutes, stirring occasionally, until the jackfruit is
+              tender and easily pulled apart with a fork.
+            </li>
+            <li>
+              Assemble the Sandwiches: Using two forks, shred the jackfruit to
+              create a "pulled" texture. Serve the pulled jackfruit on
+              whole-grain buns with coleslaw, if desired.
+            </li>
+            <li>
+              Serving Suggestion: These sandwiches pair well with sweet potato
+              fries or a fresh salad.
+            </li>
+          </ol>
+        </div>
       </div>
       <div>
-        <h3 class="jackfruitsHeadingSmall">Pulled Jackfruit BBQ Sandwiches</h3>
-        <p class="jackfruitsText">Intructions:</p>
-        <ul class="jackfruitsText">
-          <li>
-            Prepare the Jackfruit: Drain and rinse the jackfruit, then cut it
-            into smaller pieces. Remove any hard seeds and set the jackfruit
-            aside.
-          </li>
-          <li>
-            Cook the Onion and Garlic: In a large skillet, heat olive oil over
-            medium heat. Add the sliced onions and cook until soft and
-            caramelized, about 5-7 minutes. Add the minced garlic and cook for
-            another 1-2 minutes.
-          </li>
-          <li>
-            Add Spices and Jackfruit: Stir in the smoked paprika, chili powder,
-            cumin, salt, and pepper. Add the jackfruit pieces and toss to coat
-            them in the spice mixture.
-          </li>
-          <li>
-            Simmer in BBQ Sauce: Pour in the BBQ sauce, mixing well to ensure
-            the jackfruit is evenly coated. Reduce the heat and let it simmer
-            for 20-30 minutes, stirring occasionally, until the jackfruit is
-            tender and easily pulled apart with a fork.
-          </li>
-          <li>
-            Assemble the Sandwiches: Using two forks, shred the jackfruit to
-            create a "pulled" texture. Serve the pulled jackfruit on whole-grain
-            buns with coleslaw, if desired.
-          </li>
-          <li>
-            Serving Suggestion: These sandwiches pair well with sweet potato
-            fries or a fresh salad.
-          </li>
-        </ul>
-      </div>
-    </div>
-    <div>
-      <div>
-        <v-img src="../../assets/images/res2.png"></v-img>
-      </div>
-      <div>
-        <h3>JackFruit Curry</h3>
+        <div class="my-3">
+          <v-img src="../../assets/images/res2.png"></v-img>
+        </div>
+        <div>
+          <h3 class="jackfruitsHeadingSmall">JackFruit Curry</h3>
 
-        <ul>
-          <li>
-            Prepare the Jackfruit: If using canned jackfruit, drain and rinse it
-            thoroughly. Cut into bite-sized pieces.
-          </li>
-          <li>
-            Cook the Aromatics: Heat vegetable oil in a large pot over medium
-            heat. Add the cumin seeds and let them sizzle for a few seconds
-            until fragrant. Add the diced onions and cook until translucent,
-            about 5 minutes. Stir in the garlic and ginger, cooking for another
-            2 minutes.
-          </li>
-          <li>
-            Add Spices: Stir in the curry powder, turmeric, salt, and pepper,
-            cooking for 1-2 minutes to toast the spices.
-          </li>
-          <li>
-            Simmer the Curry: Add the jackfruit pieces to the pot and stir to
-            coat them with the spice mixture. Pour in the diced tomatoes and
-            tomato paste, mixing well. Add the coconut milk and bring the
-            mixture to a simmer. Reduce the heat and let the curry cook for
-            25-30 minutes, stirring occasionally, until the jackfruit is tender
-            and the sauce has thickened.
-          </li>
-          <li>
-            Serve: Garnish the curry with fresh cilantro and serve over basmati
-            rice.
-          </li>
-          <li>
-            Serving Suggestion: Enjoy this curry with warm naan bread or a side
-            of steamed vegetables.
-          </li>
-        </ul>
-      </div>
-    </div>
-    <div>
-      <div>
-        <v-img src="../../assets/images/res3.png"></v-img>
+          <ol class="jackfruitsText px-4">
+            <li>
+              Prepare the Jackfruit: If using canned jackfruit, drain and rinse
+              it thoroughly. Cut into bite-sized pieces.
+            </li>
+            <li>
+              Cook the Aromatics: Heat vegetable oil in a large pot over medium
+              heat. Add the cumin seeds and let them sizzle for a few seconds
+              until fragrant. Add the diced onions and cook until translucent,
+              about 5 minutes. Stir in the garlic and ginger, cooking for
+              another 2 minutes.
+            </li>
+            <li>
+              Add Spices: Stir in the curry powder, turmeric, salt, and pepper,
+              cooking for 1-2 minutes to toast the spices.
+            </li>
+            <li>
+              Simmer the Curry: Add the jackfruit pieces to the pot and stir to
+              coat them with the spice mixture. Pour in the diced tomatoes and
+              tomato paste, mixing well. Add the coconut milk and bring the
+              mixture to a simmer. Reduce the heat and let the curry cook for
+              25-30 minutes, stirring occasionally, until the jackfruit is
+              tender and the sauce has thickened.
+            </li>
+            <li>
+              Serve: Garnish the curry with fresh cilantro and serve over
+              basmati rice.
+            </li>
+            <li>
+              Serving Suggestion: Enjoy this curry with warm naan bread or a
+              side of steamed vegetables.
+            </li>
+          </ol>
+        </div>
       </div>
       <div>
-        <h3 class="jackfruitsHeadingSmall">Jackfruit "Chicken" Salad</h3>
+        <div class="my-3">
+          <v-img src="../../assets/images/res3.png"></v-img>
+        </div>
+        <div>
+          <h3 class="jackfruitsHeadingSmall">Jackfruit "Chicken" Salad</h3>
 
-        <ul class="jackfruitsText">
-          <li>
-            Prepare the Jackfruit: Drain and rinse the jackfruit, then shred it
-            into smaller pieces using your hands or a fork.
-          </li>
-          <li>
-            Mix the Salad: In a large bowl, combine the shredded jackfruit,
-            celery, red onion, cranberries (or grapes), and nuts.
-          </li>
-          <li>
-            Prepare the Dressing: In a small bowl, whisk together the vegan
-            mayonnaise, Dijon mustard, lemon juice, apple cider vinegar, salt,
-            and pepper.
-          </li>
-          <li>
-            Combine and Serve: Pour the dressing over the jackfruit mixture and
-            toss until everything is evenly coated. Adjust seasoning to taste.
-            Serve the salad in lettuce wraps, on whole-grain bread, or in a
-            wrap.
-          </li>
-        </ul>
-        <p>
-          Serving Suggestion: This salad makes a great light lunch or snack.
-          Pair it with a fresh fruit salad or a cup of soup for a complete meal
-        </p>
+          <ol class="jackfruitsText px-4">
+            <li>
+              Prepare the Jackfruit: Drain and rinse the jackfruit, then shred
+              it into smaller pieces using your hands or a fork.
+            </li>
+            <li>
+              Mix the Salad: In a large bowl, combine the shredded jackfruit,
+              celery, red onion, cranberries (or grapes), and nuts.
+            </li>
+            <li>
+              Prepare the Dressing: In a small bowl, whisk together the vegan
+              mayonnaise, Dijon mustard, lemon juice, apple cider vinegar, salt,
+              and pepper.
+            </li>
+            <li>
+              Combine and Serve: Pour the dressing over the jackfruit mixture
+              and toss until everything is evenly coated. Adjust seasoning to
+              taste. Serve the salad in lettuce wraps, on whole-grain bread, or
+              in a wrap.
+            </li>
+          </ol>
+          <p>
+            Serving Suggestion: This salad makes a great light lunch or snack.
+            Pair it with a fresh fruit salad or a cup of soup for a complete
+            meal
+          </p>
+        </div>
       </div>
     </div>
   </div>
