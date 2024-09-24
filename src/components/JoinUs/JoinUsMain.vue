dc90abb98eca3f63140d258c696778dca38364a0
Zain Malik <116706625+zainmalik33543@users.noreply.github.com>
2024-09-24 10:23:28 -0700

    Update JoinUsMain.vue

--- src/components/JoinUs/JoinUsMain.vue
+++ src/components/JoinUs/JoinUsMain.vue
@@ -1,5 +1,96 @@
 <template>
+  <div class="text-black px-16">
     <div>
-        <h1>join us</h1>
+      <h2 class="jackfruitsHeading mx-auto max-700">Join Our Community</h2>
+      <p class="jackfruitsText mx-auto text-center mb-6 max-900">
+        At Jackfruit Guys, we’re not just building a brand; we’re cultivating a
+        movement centered around innovation, sustainability, and growth. Our
+        vision is to bring the incredible potential of jackfruit to the
+        forefront of the food industry, tapping into its versatility and health
+        benefits to create products that are good for people and the planet.
+      </p>
+      <p class="jackfruitsText mx-auto text-center mb-6 max-900">
+        As we expand our offerings and reach new markets, we recognize the power
+        of collaboration. This is why we’re extending a unique opportunity for
+        investors who share our passion and commitment to sustainable business
+        practices. By joining us, you’ll be part of a fast-growing brand that’s
+        disrupting the plant-based food industry and driving positive change.
+      </p>
+      <p class="jackfruitsText mx-auto text-center mb-6 max-900">
+        We are seeking investors who believe in the potential of our products
+        and want to be part of our journey towards becoming a household name.
+        Your investment will help us scale operations, enhance our product line,
+        and broaden our distribution, enabling us to reach more customers and
+        make a larger impact.
+      </p>
+      <p class="jackfruitsText mx-auto text-center mb-6 max-900">
+        Together, we can elevate Jackfruit Guys to new heights—creating a
+        lasting legacy in the plant-based food sector. Let’s grow this exciting
+        future, one opportunity at a time!
+      </p>
     </div>
+    <div>
+      <v-img src="../../assets/images/join1.png"></v-img>
+    </div>
+    <div>
+      <v-row>
+        <v-col cols="6"
+          ><div>
+            <h2 class="jackfruitsHeading text-left max-700">
+              Why Partner with Jackfruit Guys?
+            </h2>
+            <p class="jackfruitsText text-left max-900">
+              At Jackfruit Guys, our values guide everything we do. We are
+              committed to:
+            </p>
+            <p class="jackfruitsText text-left max-900">
+              Partnering with Jackfruit Guys means joining a unique bridge
+              between farmers and consumers. We’re dedicated to transforming
+              jackfruit into healthy, delicious products while ensuring that
+              farmers receive fair support and recognition. By investing with
+              us, you’ll help us connect high-quality, sustainable produce
+              directly to people who care about their health and the
+              environment. Your support will empower us to expand our reach,
+              enhance our offerings, and make a meaningful difference in the
+              food industry. Join us and be part of a team that’s making a real
+              impact from farm to table.
+            </p>
+            <p class="jackfruitsText text-left max-900">
+              If you’re interested in learning more about how you can partner
+              with us, please don’t hesitate to reach out. We’d love to discuss
+              potential opportunities.
+            </p>
+            <p class="jackfruitsText text-left max-900">
+              <b>Contact us at: </b>
+            </p>
+            <p class="jackfruitsText text-left max-900">
+              sales@jackfruitguys.com
+            </p>
+            <p class="jackfruitsText text-left max-900">+52 644 246 6071</p>
+            <p class="jackfruitsText text-left max-900">
+              info@jackfruitguys.com
+            </p>
+            <p class="jackfruitsText text-left max-900">+1 818 423 3779</p>
+            <p class="jackfruitsText text-left max-900">
+              <b>Let’s grow this exciting future, one opportunity at a time!</b>
+            </p>
+          </div></v-col
+        >
+        <v-col cols="6"
+          ><div>
+            <v-img src="../../assets/images/join2.png"></v-img></div
+        ></v-col>
+      </v-row>
+    </div>
+  </div>
+  <div>
+    <div
+      class="background-green py-16 text-center d-flex justify-space-between"
+    >
+      <h3 class="lastHeading">
+        Join us today and be part of something bigger!
+      </h3>
+      <v-btn class="contactbtn my-5" elevation="0">Contact Us</v-btn>
+    </div>
+  </div>
 </template>
