dda8630f03a267e0927fa8f8ede406258e7fb147
Zain Malik <116706625+zainmalik33543@users.noreply.github.com>
2024-10-13 08:42:23 -0700

    Blogs Added

--- src/components/BlogsPage/MainBlogs.vue
+++ src/components/BlogsPage/MainBlogs.vue
@@ -4,14 +4,20 @@
       <v-col
         v-for="blog in blogs"
         :key="blog.id"
-        cols="12" lg="6" xl="6"
+        cols="12"
+        lg="6"
+        xl="6"
         style="cursor: pointer"
       >
         <div @click="goToDetail(blog.id)">
-          <v-img max-height="80vh" class="joinusimgCss" :src="getImageUrl(blog.image)"></v-img>
+          <v-img
+            min-height="80vh"
+            class="joinusimgCss"
+            :src="blog.image_1"
+          ></v-img>
           <h3 class="jackfruitsHeadingSmall pt-3">{{ blog.title }}</h3>
           <p class="jackfruitsText py-5">
-            {{ blog.description.slice(0, 150) }}...
+            {{ blog.subtitle.slice(0, 150) }}...
           </p>
 
           <p class="jackfruitsTextGreen">Read More...</p>
@@ -22,16 +28,51 @@
 </template>
 
 <script>
-import blogs from "./Blogs.json";
+// import blogs from "./Blogs.json";
 import { useRouter } from "vue-router";
+import { HTTP } from "../../common/commom-http";
+import { APP_URL } from "../../common/commom-http";
 
 export default {
   data() {
     return {
-      blogs,
+      blogs: [],
     };
   },
+  watch: {
+    // Watch for changes in the route (URL)
+    "$route.query.language_id": {
+      handler(newValue) {
+        this.getAboutData(newValue);
+      },
+      immediate: true, // Call the handler right away
+    },
+  },
   methods: {
+    async getAboutData(language_id = 1) {
+      const payload = {
+        language_id: language_id || this.$route.query.language_id || 1, // Default to 1 if no language_id in URL
+      };
+      try {
+        const response = await HTTP.post("blogs", payload);
+        console.log("response of the blog", response.data.data);
+        this.blogs = response.data.data;
+        this.blogs.forEach((section) => {
+          section.image_1 = `${APP_URL}${section.image_1}`;
+        });
+        // this.sectionOne = response.data.data.AboutSection1;
+        // this.sectionTwo = response.data.data.AboutSection2;
+        // this.sectionThree = response.data.data.AboutSection3;
+        // this.sectionFour = response.data.data.AboutSection4;
+        // Updating the Image with the base url
+        // this.sectionOne.image = `${APP_URL}${this.sectionOne.image}`;
+        // this.sectionTwo.image = `${APP_URL}${this.sectionTwo.image}`;
+        // this.sectionThree.image = `${APP_URL}${this.sectionThree.image}`;
+        // this.sectionFour.image = `${APP_URL}${this.sectionFour.image}`;
+      } catch (error) {
+        console.log("error", error);
+      }
+    },
     getImageUrl(imagePath) {
       return new URL(`${imagePath}`, import.meta.url).href;
     },
