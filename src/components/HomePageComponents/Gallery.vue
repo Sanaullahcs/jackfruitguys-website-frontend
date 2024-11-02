a225366d62462646f0ed43a49afbfb19e6f69e40
Zain Malik <116706625+zainmalik33543@users.noreply.github.com>
2024-11-02 22:09:08 +0500

    Update Gallery.vue

--- src/components/HomePageComponents/Gallery.vue
+++ src/components/HomePageComponents/Gallery.vue
@@ -1,8 +1,17 @@
 <template>
   <div class="stack text-black">
-    <!-- {{ sectionTwo }} -->
-    <div class="stack__card" v-for="(card, index) in sectionTwo" :key="index">
-      <img :src="card.media" alt="card image" max-height="80vh" class="card-img joinusimgCss" />
+    <div
+      class="stack__card"
+      v-for="(card, index) in sectionTwo"
+      :key="index"
+      ref="cards"
+    >
+      <img
+        :src="card.media"
+        alt="card image"
+        max-height="80vh"
+        class="card-img joinusimgCss fade-in"
+      />
     </div>
   </div>
 </template>
@@ -12,20 +21,29 @@ export default {
   props: {
     sectionTwo: {
       type: Array,
-      required: true, // Set to true if the prop is required
+      required: true,
     },
   },
-  data() {
-    return {
-      // cards: [
-      //   "../../assets/images/gal1.png",
-      //   "../../assets/images/gal2.png",
-      //   "../../assets/images/gal3.png",
-      //   "../../assets/images/gal4.png",
-      //   "../../assets/images/gal1.png",
-      // ],
-      // cardsColors: ["#E3342F", "#38C172", "#3490DC", "#F66D9B", "#F6993F"],
-    };
+  mounted() {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            entry.target.classList.add("animate");
+          } else {
+            entry.target.classList.remove("animate");
+          }
+        });
+      },
+      {
+        threshold: 0.1, // Trigger animation when 10% of the image is in view
+      }
+    );
+
+    // Observe each card image
+    this.$refs.cards.forEach((card) => {
+      observer.observe(card.querySelector("img"));
+    });
   },
 };
 </script>
@@ -33,7 +51,16 @@ export default {
 <style scoped>
 .card-img {
   width: 100%;
+  opacity: 0;
+  transform: translateY(20px);
+  transition: opacity 0.6s ease, transform 0.6s ease;
 }
+
+.card-img.animate {
+  opacity: 1;
+  transform: translateY(0);
+}
+
 html {
   font-family: sans-serif;
   box-sizing: border-box;
@@ -46,40 +73,4 @@ html {
   padding: 0;
   box-sizing: inherit;
 }
-
-.stack {
-  display: flex;
-  flex-direction: column;
-}
-
-.stack__card {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  /* height: 100vh; */
-  position: sticky;
-  top: 0;
-  box-shadow: 0 -1px 3px rgba(0, 0, 0, 0.1), 0 -1px 2px rgba(0, 0, 0, 0.25);
-  font-size: 10rem;
-}
-
-/* .stack__card:nth-child(1) {
-  background-color: #e3342f;
-}
-
-.stack__card:nth-child(2) {
-  background-color: #38c172;
-}
-
-.stack__card:nth-child(3) {
-  background-color: #3490dc;
-}
-
-.stack__card:nth-child(4) {
-  background-color: #f66d9b;
-}
-
-.stack__card:nth-child(5) {
-  background-color: #f6993f;
-} */
 </style>
